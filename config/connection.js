f41032bd54b81e554f5177bb542d2b689a716931
philmao <philip_mao@yahoo.com>
2017-07-29 00:31:48 -0700

    remove node_modules, update connection.js for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "phil",
-  password: "p4ssword",
-  database: "burgerDB"
-});
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'phil',
+    password: 'p4ssword',
+    database: 'burgerdb'
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
